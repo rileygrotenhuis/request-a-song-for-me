8808b4f7563c1cb63862a88eed22860c9edbbc1d
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2023-02-10 16:46:13 -0600

    feat: fetch client user access token

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,23 @@
-export default function Page() {
+async function getData() {
+    const res = await fetch('https://accounts.spotify.com/api/token?grant_type=client_credentials', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic MTIzNTNhY2IwMDE1NDNmY2E4YmYzM2ZhZTVmNTk4NDI6ZGViMDcxNzNhNmQxNDZhNjg0NjRhMDFlYTU2ZDBjNDY=',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+}
+
+export default async function Page() {
+    const data = await getData();
+    const userSpotifyAccessToken = data.access_token;
+
     return (
         <>
             <h1>Testing</h1>
