3f669a816cecbc2f153c7c4e6620063b377730bd
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2023-02-10 23:18:41 -0600

    feat: add dark mode theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,24 @@
 
 import { RecoilRoot } from 'recoil';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+});
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head />
             <RecoilRoot>
-                <body>{children}</body>
+                <ThemeProvider theme={darkTheme}>
+                    <CssBaseline />
+                    <body>{children}</body>
+                </ThemeProvider>
             </RecoilRoot>
         </html>
     );
