69803b08fc88ee0a10900760d186b2654f175951
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2023-02-10 22:52:34 -0600

    fix: remove unused code

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,15 +1,9 @@
-'use client';
-
-import { useRecoilState } from 'recoil';
-import { spotifySearchResults } from '@/atoms/spotifySearchResults';
+"use client";
 
 import SearchForm from './SearchForm';
 import SearchResults from './SearchResults';
 
 export default function SearchContainer(props) {
-    const [searchResults, setSearchResults] =
-        useRecoilState(spotifySearchResults);
-
     return (
         <>
             <SearchForm {...props} />
